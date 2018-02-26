fcd6d0d7d032aa4397c0070ec426be7386b4c85e
Denis Borisov <borisovdenis-kb@yandex.ru>
2018-02-26 21:30:12 +0400

    Remove unused dependencies from QuestionController.

--- src/main/java/ru/intodayer/quizproject/ws/controller/AdminWebSocketController.java
+++ src/main/java/ru/intodayer/quizproject/ws/controller/AdminWebSocketController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import ru.intodayer.quizproject.dto.DtoConverter;
 import ru.intodayer.quizproject.service.QuestionService;
 import ru.intodayer.quizproject.ws.message.Command;
 import ru.intodayer.quizproject.ws.message.Message;
@@ -15,20 +13,11 @@ import ru.intodayer.quizproject.ws.message.Message;
 @Controller
 public class AdminWebSocketController {
 
-    private final SimpMessagingTemplate template;
-
     private QuestionService questionService;
 
-    private DtoConverter dtoConverter;
-
     @Autowired
-    public AdminWebSocketController(
-            SimpMessagingTemplate template,
-            QuestionService questionService,
-            DtoConverter dtoConverter) {
-        this.template = template;
+    public AdminWebSocketController(QuestionService questionService) {
         this.questionService = questionService;
-        this.dtoConverter = dtoConverter;
     }
 
     @MessageMapping("/app/admin/getCommand")
